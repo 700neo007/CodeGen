7a07cac78e621d5bbc23915ea2b8ecde594f2ed2
SheZaiPC <huanongweiky@yeah.net>
2013-09-20 14:23:57 +0800

    build project

---
+++ CodeGen-common/src/main/java/org/CodeGen/common/App.java
@@ -0,0 +1,13 @@
+package org.CodeGen.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
